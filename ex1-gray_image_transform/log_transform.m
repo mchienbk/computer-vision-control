00518022edf7d438e01bfce164ac2d6e076eea49
Tran Minh Chien <minhchien2031995@gmail.com>
2020-03-03 10:14:37 +0700

    Update log_transform.m

--- ex1-gray_image_transform/log_transform.m
+++ ex1-gray_image_transform/log_transform.m
@@ -1,25 +1,23 @@
-# =========================================================================
-# Filename:     power_trans.py
-#               Image Power Transform
-# Name:         Tran Minh Chien - ID: 1870324
-# Subject:      Computer Vision and Control - Exercise 1
-# Date:         2019.08.30
-# =========================================================================
-
-import numpy as np
-import cv2
-
-def main():
-    # Load the image
-    img = cv2.imread('D:/sample.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray',gray)
-
-    img2 = np.array(255*(gray/255)**0.4,dtype='uint8')
-    # Display the images in subplots
-    cv2.imshow('img2',img2)
-
-if __name__ == '__main__':
-    main()
-   
-    cv2.waitKey(0)
+% =========================================================================
+% Filename:     log_transorm.m
+% Name:         Tran Minh Chien - ID: 1870324
+% Subject:      Computer Vision and Control - Exercise 1
+% Date:         2019.08.30
+% =========================================================================
+clc; clear all; close all;
+im=imread('sample.jpg');
+g=rgb2gray(im);
+c=input('Enter the constant value, c = ');
+[M,N]=size(g);
+        for x = 1:M
+            for y = 1:N
+                r=g(x,y);
+                r=double(r);
+                s(x,y)=c*log10(1+r);
+                if (s(x,y) > 255)
+                    s(x,y) = 255;
+                end
+            end
+        end
+s = mat2gray(s);
+imshow(g), figure, imshow(s);
